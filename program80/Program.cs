d9638fad9bafcc760fcd6ca45123afe607bee737
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-06-20 17:28:22 +0300

    Add project files.

---
+++ program80/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace program80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string number = Console.ReadLine();
+
+            while ( number != "x")
+            {
+                int sum = Convert.ToInt32(number);
+                count += sum;
+                number = Console.ReadLine();
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
